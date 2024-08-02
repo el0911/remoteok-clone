eb9035e5eef973daf26721be307782f2da1c4488
el <somto.achu5@gmail.com>
2024-08-02 15:26:23 +0100

    added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import "@/styles/globals.css";
 import type { Metadata } from "next";
-
+import { Analytics } from "@vercel/analytics/react"
 import { nunito, pacifico } from "@/styles/fonts";
 import AuthProvider from "../context/AuthProvider";
 
@@ -20,6 +20,7 @@ export default function RootLayout({
         <AuthProvider>
           <main className="relative min-h-screen">{children}</main>
         </AuthProvider>
+        <Analytics />
       </body>
     </html>
   );
